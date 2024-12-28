37e029ad5e1e8cb0df257c3779bda7dfbc3b0e3f
DheerajSuthar001 <dheerajsuthar.it21@gmail.com>
2024-12-28 11:44:56 +0530

    Learnt different version of useEffect

--- src/App.js
+++ src/App.js
@@ -3,17 +3,52 @@ import './App.css';
 
 function App() {
   const [newText,setnewText]=useState('');
-  useEffect(() => {
-    console.log('UI rendering done');
-  })
+
+  //Everytime Ui renders
+  // useEffect(() => {
+  //   console.log('UI rendering done');
+  // });
+
+  //first and one time only
+  // useEffect (()=>{
+  //   console.log('UI rendering done');
+  // },[]);
+
+  //first time and dependency based
+  // useEffect(()=>{
+  //   console.log('UI rendering done');
+  // },[newText]);
+
+  //unmounting
+  useEffect (()=>{
+    console.log('added');
+
+    return ()=>{
+      console.log('removed');
+    }
+  },[newText])
   function changeHandler(event) {
     console.log(event.target.value);
     setnewText(event.target.value);
-  }
+  };
+
+  const [windowWidth, setwindowWidth]=useState(window.innerWidth);
+  useEffect(()=>{
+    let handlewindowSize=()=>{
+      setwindowWidth(window.innerWidth);
+    }
+    window.addEventListener('resize',handlewindowSize);
+    return ()=>{
+      window.removeEventListener('resize',handlewindowSize);
+      
+    }
+  },[windowWidth]);
+
   return (
     <div>
       <input type='text' onChange={changeHandler}></input>
       <p>{newText}</p>
+      <p>Window width is {windowWidth}</p>
     </div>
 
   );
